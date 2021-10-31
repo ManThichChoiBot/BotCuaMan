dc41ba08100e841a010d11889aa80bd97806f275
Phạm Gia Mẫn <93466616+ManThichChoiBot@users.noreply.github.com>
2021-10-31 17:56:30 +0700

    Add files via upload

---
+++ [WINDOW] START.bat
@@ -0,0 +1,7 @@
+# Author : Khoa Ko Mlem
+# Copyright (c) khoakomlem
+# Script chạy bot
+echo "Dang chay mirai bot bootloader . . ."
+chcp 65001 > NUL
+npm start
+read -p "Nhan phim ENTER de thoat . . ."
